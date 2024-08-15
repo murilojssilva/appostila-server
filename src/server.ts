556ff18fdbe1e2b2bc3c7d13dfad628d17d14b15
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2024-08-14 21:27:27 -0300

    Fix port

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import { app } from './app'
 import { env } from './env'
 
+const port = env.PORT || 3000
+
 app
   .listen({
-    port: env.PORT,
+    port,
+    host: '0.0.0.0',
   })
   .then(() => {
-    console.log('HTTP Server Running')
+    console.log(`HTTP Server Running on port ${port}`)
+  })
+  .catch((err) => {
+    console.error('Error starting server:', err)
+    process.exit(1)
   })
